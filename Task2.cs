a984b91483d661feaf467e751bb622ac3401b4d8
Grigoriy <38793502+Grepshe@users.noreply.github.com>
2021-07-09 15:12:26 +0300

    Task2 added

---
+++ Task2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    public enum Severity
+    {
+        Trace,
+        Debug,
+        Info,
+        Warn,
+        Error,
+        Criti
+    }
+
+    public class Logger : IDisposable
+    {
+        public Logger(string path)
+        {
+            m_sW = new StreamWriter(path, true, System.Text.Encoding.Default);
+        }
+
+        public void Log(string data, Severity severity)
+        {
+            m_sW.WriteLine($"[{DateTime.Now}] [{severity}]: {data}");
+            m_sW.Flush();
+        }
+
+        public void Dispose()
+        {
+            m_sW.Close();
+            m_sW.Dispose();
+        }
+
+        private StreamWriter m_sW;
+    }
+    class Task2
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger("log.txt");
+            logger.Log("Gg, proizoshol vzriv", Severity.Debug);
+        }
+    }
+}
